9ef809d04731bcab6e247e5523a0bdfb98f0dcce
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2023-05-30 20:22:04 +0100

    first commit

---
+++ src/context/books.js
@@ -0,0 +1,57 @@
+import { createContext, useState } from "react";
+import axios from "axios";
+
+const BooksContext = createContext();
+
+function Provider({ children }) {
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
+    setBooks(updatedBooks);
+  };
+
+  const value = {
+    books,
+    fetchBooks,
+    editBookById,
+    deleteBookById,
+    createBook,
+  };
+
+  return (
+    <BooksContext.Provider value={value}>{children}</BooksContext.Provider>
+  );
+}
+
+export { Provider };
+export default BooksContext;
